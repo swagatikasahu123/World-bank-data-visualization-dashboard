af85f60c7ac77c4c1557e46d43110fcc302e06f3
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2026-03-09 10:43:14 +0530

    update cors for vercel domain

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://your-frontend-domain.vercel.app",
+    "https://world-bank-data-visualization-dashb.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
